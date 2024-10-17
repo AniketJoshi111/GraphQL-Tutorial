2bbd570ac1a5a8c498546ea10f8f71541e3402d5
AniketJoshi111 <aniketjoshi2606@gmail.com>
2024-10-17 10:05:30 +0530

    basic resolvers and typedefs learned by handson

--- server/index.js
+++ server/index.js
@@ -3,23 +3,44 @@ const {ApolloServer} = require('@apollo/server');
 const {expressMiddleware} = require('@apollo/server/express4');
 const bodyParser = require("body-parser");
 const cors = require('cors');
-const bodyParser = require('body-parser');
+const { title } = require('process');
+const {default: axios} = require('axios');
 
 async function startServer() {
     const app = express();
     const server = new ApolloServer({
+
         typeDefs:`
+            type User {
+                id: ID!
+                name:String!
+                username:String!
+                email:String!
+                phone:String!
+                website:String!
+            }
             type Todo {
                id:ID!,
                title:String!
                completed:Boolean
+               user:UserID!
             }
 
             type Query{
-               getTodos:[Todo]!
+               getTodos:[Todo]
+               getAllUsers:[User]
+               getUserbyID(id:ID!):User
             }
         `,
         resolvers:{
+            Todo:{
+                user: async (todo)=>await  axios.get(`https://jsonplaceholder.typicode.com/users/${todo.id}`)).data
+            }
+            Query: {
+                getTodos: async () => (await axios.get('https://jsonplaceholder.typicode.com/todos')).data;
+                getAllUsers: async ()=> (await axios.get('https://jsonplaceholder.typicode.com/users')).data;
+                getUserbyID: async (parent,{id}) => (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)).data;
+            }  
         }
     });
 
@@ -33,3 +54,4 @@ async function startServer() {
     })
     
 }
+startServer();
